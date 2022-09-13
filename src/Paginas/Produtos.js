806fc597aa98b70e18654c4d0b08595891b12f67
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-09-13 15:29:57 -0300

    Começo da tela de cadastro de produtos

--- src/Paginas/Produtos.js
+++ src/Paginas/Produtos.js
@@ -48,7 +48,8 @@ const PGProdutos = () => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '80%',
+        width: '90%',
+        height:'90%',
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
@@ -83,12 +84,8 @@ const PGProdutos = () => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
+                    <label>Nome: </label>
+                    <input/>
                 </Box>
             </Modal>
 
