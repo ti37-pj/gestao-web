fbe49a4334fc8ecde4c2483be76a55212df2e05a
joaomotz <106111080+joaomotz@users.noreply.github.com>
2022-09-27 16:45:48 -0300

    Cadastra e Altera produto completo

--- src/Componentes/ProdutoCard/index.js
+++ src/Componentes/ProdutoCard/index.js
@@ -4,16 +4,29 @@ import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import {CardContent, CardHeader, Switch, Typography,FormControlLabel, Button } from "@mui/material";
 import styles from './styles.module.css';
-import TextField from '@mui/material/TextField';
+import ProdutoModal from "../ProdutoModal";
 
 
 
 
-const ProdutoCard = ({produto}) => {
+
+const ProdutoCard = (props) => {
 
     const [apareceMenu, setApareceMenu] = useState(false);
+    const [produto, setProduto] = useState(props.produto);
 
-    const [editando, setEditando] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const alteraProduto = (produtoAlterado) => {
+        //api.put("/produtos", produtoAlterado)
+        //.then((res) => {
+        //    if (res.status === 201 || res.status === 200) {
+                setModalOpen(false)
+                setProduto(produtoAlterado)
+        //    }
+        //})
+        //.catch();
+    }
 
     return(
         <div className={styles.produtoCard}>
@@ -35,45 +48,32 @@ const ProdutoCard = ({produto}) => {
                     alt="Foto do produto"
                 />
                 <CardContent>
-                    {produto.editando != null ? 
-                    <>
-
-                        <TextField id="standard-basic" value={produto.nome} label="Standard" variant="standard" />
-                        <TextField id="standard-basic" value={produto.descricao} label="Standard" variant="standard" />
-                        <TextField id="standard-basic" value={produto.preco_custo} label="Standard" variant="standard" />
-                        <TextField id="standard-basic" value={produto.preco_venda} label="Standard" variant="standard" />
-                        <TextField id="standard-basic" value={produto.categoria} label="Standard" variant="standard" />
-
-                    </>              
-                    :
-                    <>
-                        <Typography>
-                            {produto.nome}
-                        </Typography>
-                        <Typography>
-                            {produto.descricao}
-                        </Typography>
-                        <Typography>
-                            Preço Custo:R${produto.preco_custo}
-                        </Typography>
-                        <Typography>
-                            Preço Venda:R${produto.preco_venda}
-                        </Typography>
-                        <Typography>
-                            Categoria:{produto.categoria}
-                        </Typography>
-                    </>          
-                }
+                    <Typography>
+                        {produto.nome}
+                    </Typography>
+                    <Typography>
+                        {produto.descricao}
+                    </Typography>
+                    <Typography>
+                        Preço Custo:R${produto.preco_custo}
+                    </Typography>
+                    <Typography>
+                        Preço Venda:R${produto.preco_venda}
+                    </Typography>
+                    <Typography>
+                        Categoria:{produto.id_categoria}
+                    </Typography>
                     <Typography>
                         {produto.registro}
                     </Typography>    
                 </CardContent>
                 <CardActions>
-                <Button onClick={() => produto.editando = true}>
+                <Button onClick={() => {setModalOpen(true)}}>
                     Editar
                 </Button>
                 </CardActions>
             </Card>
+            <ProdutoModal open={modalOpen} onClose={() => {setModalOpen(false)}} onSave={(produto) => {alteraProduto(produto)}} produto={produto} />
         </div>
     
     )
--- src/Paginas/Produtos.js
+++ src/Paginas/Produtos.js
@@ -1,73 +1,39 @@
 import React, { useEffect } from 'react';
 import ProdutoCard from '../Componentes/ProdutoCard';
-import axios from 'axios';
 import Cards from '../Componentes/Cards/Index';
-import {Button, CircularProgress} from '@mui/material';
+import {CircularProgress} from '@mui/material';
 import FabAdd from '../Componentes/FabAdd';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
+import api from '../api';
+import ProdutoModal from '../Componentes/ProdutoModal';
 
 const PGProdutos = () => {
 
     useEffect(()=>{
-        axios.get("http://10.60.46.31:3001/produtos/busca_todos")
-            .then(
-                res => mudaProdutos(res.data)
-            )
-            .catch(res => console.log(res))
-
-
+        buscaTodos();
     }, [])
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '80%',
-        height: '65%',
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
-
-    const styleText = {
-        border: '2px solid #000',
-        padding: '10px',
-        margin: '10px',
-        width: '99%',
+    const buscaTodos = () => {
+        api.get("/produtos/busca_todos")
+        .then(res => setProdutos(res.data))
+        .catch(res => console.log(res));
     }
 
-
-    const cadastraProduto = (produtos, event) => {
-        axios.post("http://10.60.46.31:3001/produtos/busca_todos")
-        event.preventDefault();
-
-        setLista(lista => [...lista,
-			{
-                nome: produtos.nome
-                descricao: produtos.descricao
-                imagem: produtos.imagem_url
-                preco_custo: produtos.preco_custo
-                preco_venda: produtos.preco_venda
-			}
-	] );
-
+    const cadastraProduto = (produto) => {
+        api.post("/produtos/insere", produto)
+        .then((res) => {
+            if (res.status === 201 || res.status === 200) {
+                buscaTodos();
+            }
+        })
+        .catch();
     }
 
-    const [produtos, mudaProdutos] = React.useState([])
-
-    const [nome, setNome] = React.useState();
-    const [descricao, setDescricao] = React.useState();
-    const [imagem, setimagem] = React.useState();
-    const [preco_custo, setPreco_custo] = React.useState();
-    const [preco_venda, setPreco_Venda] = React.useState();
-
+    const [produtos, setProdutos] = React.useState([])
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
+
 
     return ( 
         <div>
@@ -76,76 +42,13 @@ const PGProdutos = () => {
                 <CircularProgress />
             ) : (
                 <Cards>
-                    {produtos.map(produto => <ProdutoCard produto={produto}/>)}
+                    {produtos.map(produto => <ProdutoCard produto={produto} key={produto.id} />)}
                 </Cards>
             )}
 
             <FabAdd onClick={handleOpen} />
 
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <TextField 
-                    sx={styleText}
-                    type="text"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                    id="standard-basic"
-                    label="Insira o Nome do Produto"
-                    variant="standard" />
-
-                    <TextField 
-                    sx={styleText}
-                    type="text"
-                    value={descricao}
-                    onChange={(e) => setDescricao(e.target.value)}
-                    id="standard-basic"
-                    label="Insira a Descrição do Produto"
-                    variant="standard" />
-
-                    <TextField 
-                    sx={styleText}
-                    type="text"
-                    value={preco_venda}
-                    onChange={(e) => setPreco_Venda(e.target.value)}
-                    id="standard-basic"
-                    label="Insira o Preço de Venda do Produto"
-                    variant="standard" />
-
-                    <TextField 
-                    sx={styleText}
-                    type="text"
-                    value={preco_custo}
-                    onChange={(e) => setPreco_custo(e.target.value)}
-                    id="standard-basic"
-                    label="Insira o Preço de Custo do Produto"
-                    variant="standard" />
-
-                    <TextField 
-                    sx={styleText}
-                    type="text" 
-                    id="standard-basic"
-                    label="Insira a Categoria do Produto"
-                    variant="standard" />
-
-                    <TextField 
-                    sx={styleText}
-                    type="text"
-                    id="standard-basic"
-                    value={imagem}
-                    onChange={(e) => setimagem(e.target.value)}
-                    label="Insira a URL da imagem do Produto"
-                    variant="standard" />
-
-                    <Button onSubmit={event => cadastraProduto(produtos, event)}>Salvar</Button>
-
-                    <Button>Cancelar</Button>
-                </Box>
-            </Modal>
+            <ProdutoModal open={open} onClose={handleClose} onSave={produto => cadastraProduto(produto)} />
 
         </div>
      );
